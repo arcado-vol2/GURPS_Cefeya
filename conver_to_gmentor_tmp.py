1a474907d9be7b1abbb793eed9a5672fe71bcd9d
arcado-vol2 <piterz31a@gmail.com>
2026-03-01 21:05:54 +0300

    vault backup: 2026-03-01 21:05:54

--- conver_to_gmentor_tmp.py
+++ conver_to_gmentor_tmp.py
@@ -1,8 +1,10 @@
 import os
 from pathlib import Path
+from datetime import datetime
 
-excluded = {'.git', 'README.md', 'README'}
-
+excluded = {'.git', 'README.md', 'README', 'conver_to_gmentor_tmp.py', 'out.xml'}
+creation_date = "01.03.2026 13:11"
+header_postfix = "petr"
 
 def remove_extention_from_name(original_name: str) -> str:
     return Path(original_name).stem
@@ -46,16 +48,16 @@ def scan_directory_xml(base_path: str, indent: int = 0, counters: dict = None) -
         
         if is_dir:
             # Открывающий тэг директории
-            result.append(f'{indent_str}<dir name="{original_name}" id="{unique_id}">')
+            result.append(f'{indent_str}<gm-folder style="zoom: 1;" id=board"{unique_id}"> <name icon="fa fa-fw fa-folder">{original_name}</name>')
             # Рекурсивный вызов для директорий
             sub_result, counters = scan_directory_xml(full_path, indent + 1, counters)
             if sub_result:
                 result.append(sub_result)
             # Закрывающий тэг директории
-            result.append(f'{indent_str}</dir>')
+            result.append(f'{indent_str}</gm-folder>')
         else:
             # Файл с парными тэгами
-            result.append(f'{indent_str}<file name="{original_name}" id="{unique_id}"></file>')
+            result.append(f'{indent_str}<gm-story id="{unique_id}" > <name icon="fa fa-fw fa-file-text-o">{original_name}</name>   <story-text><div>Тест</div></story-text> </gm-story>')
     
     return '\n'.join(result), counters
 
@@ -64,8 +66,8 @@ def main():
     script_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir_name = os.path.basename(script_dir)
     
-    header = f"# {parent_dir_name}"
-    
+    header = f"<gm-root>\n    <created_date>{creation_date}</created_date>\n    <modified_date>{datetime.now().strftime("%d.%m.%Y %H:%M")}</modified_date>\n    <name>{parent_dir_name}_{header_postfix}</name><portrait></portrait>\n"
+    footer = "</gm-root>"
     # Сканируем директорию
     lines, _ = scan_directory_xml(script_dir)
     
@@ -73,7 +75,7 @@ def main():
     output_text = header + '\n' + lines
     
     # Записываем в txt файл
-    output_file = os.path.join(script_dir, 'directory_structure.txt')
+    output_file = os.path.join(script_dir, 'out.xml')
     with open(output_file, 'w', encoding='utf-8') as f:
         f.write(output_text)
     
